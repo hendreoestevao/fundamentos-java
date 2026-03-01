fb36f4b91456d3b32803005fbe3aa2f7b903ca20
hendreo santos <hendreosantos1@outlook.com>
2026-03-01 12:50:55 -0300

    embeddable

--- jpa-hibernate/src/main/java/org/example/test/umpramuitos/ObterPedido.java
+++ jpa-hibernate/src/main/java/org/example/test/umpramuitos/ObterPedido.java
@@ -1,6 +1,8 @@
 package org.example.test.umpramuitos;
 
 import org.example.infra.DAO;
+import org.example.model.composicao.Endereco;
+import org.example.model.composicao.Funcionario;
 import org.example.model.umpramuitos.ItemPedido;
 import org.example.model.umpramuitos.Pedido;
 
@@ -9,7 +11,6 @@ public class ObterPedido {
         DAO<Pedido> dao = new DAO<>(Pedido.class);
 
         Pedido pedido = dao.obterPorId(1L);
-
 
         for (ItemPedido item : pedido.getItens()) {
             System.out.println(item.getQuantidade());
